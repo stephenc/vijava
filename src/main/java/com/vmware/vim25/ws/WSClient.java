e1a244a079b9b097c74d436e71657e9f0f48c4df
Stephen Connolly <stephen.alan.connolly@gmail.com>
2015-02-16 17:38:42 +0000

    Fix issue with connecting to hosted VMware ESX instances

--- src/main/java/com/vmware/vim25/ws/WSClient.java
+++ src/main/java/com/vmware/vim25/ws/WSClient.java
@@ -2,35 +2,39 @@
 Copyright (c) 2013 Steve Jin, All Rights Reserved.
 Copyright (c) 2009 VMware, Inc. All Rights Reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
+Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-* Redistributions of source code must retain the above copyright notice, 
+* Redistributions of source code must retain the above copyright notice,
 this list of conditions and the following disclaimer.
 
-* Redistributions in binary form must reproduce the above copyright notice, 
-this list of conditions and the following disclaimer in the documentation 
+* Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
 and/or other materials provided with the distribution.
 
 * Neither the name of VMware, Inc. nor the names of its contributors may be used
-to endorse or promote products derived from this software without specific prior 
+to endorse or promote products derived from this software without specific prior
 written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL VMWARE, INC. OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 ================================================================================*/
 
 package com.vmware.vim25.ws;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -53,7 +57,7 @@ import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
-/** 
+/**
  * The Web Service Engine
  * @author Steve Jin (sjin@vmware.com)
 */
@@ -66,27 +70,27 @@ final public class WSClient
   private final static String SOAP_ACTION_V50 = "urn:vim25/5.0";
   private final static String SOAP_ACTION_V51 = "urn:vim25/5.1";
   private final static String SOAP_ACTION_V55 = "urn:vim25/5.5";
-  
+
   private URL baseUrl = null;
   private String cookie = null;
   private String vimNameSpace = null;
   private String soapAction = null;
   private int connectTimeout = 0;
   private int readTimeout = 0;
-  
+
   XmlGen xmlGen = new XmlGenDom();
-  
-  public WSClient(String serverUrl) throws MalformedURLException 
+
+  public WSClient(String serverUrl) throws MalformedURLException
   {
     this(serverUrl, true);
   }
-  
-  public WSClient(String serverUrl, boolean ignoreCert) throws MalformedURLException 
+
+  public WSClient(String serverUrl, boolean ignoreCert) throws MalformedURLException
   {
     if(serverUrl.endsWith("/"))
     {
       serverUrl = serverUrl.substring(0, serverUrl.length()-1);
-    } 
+    }
     this.baseUrl = new URL(serverUrl);
     if(ignoreCert)
     {
@@ -95,7 +99,7 @@ final public class WSClient
         trustAllHttpsCertificates();
         HttpsURLConnection.setDefaultHostnameVerifier
         (
-          new HostnameVerifier() 
+          new HostnameVerifier()
           {
             public boolean verify(String urlHostName, SSLSession session)
             {
@@ -106,37 +110,37 @@ final public class WSClient
       } catch (Exception e)  {}
     }
   }
-  
+
   public Object invoke(String methodName, Argument[] paras, String returnType) throws RemoteException
   {
     String soapMsg = XmlGen.toXML(methodName, paras, this.vimNameSpace);
-    
+
     InputStream is = null;
-    try 
+    try
     {
       is = post(soapMsg);
       return xmlGen.fromXML(returnType, is);
     }
-    catch (Exception e1) 
+    catch (Exception e1)
     {
       throw new RemoteException("VI SDK invoke exception:" + e1);
     }
     finally
     {
-      if(is!=null) 
+      if(is!=null)
         try { is.close(); } catch(IOException ioe) {}
     }
   }
-  
+
   public StringBuffer invokeAsString(String methodName, Argument[] paras) throws RemoteException
   {
     String soapMsg = XmlGen.toXML(methodName, paras, this.vimNameSpace);
 
-    try 
+    try
     {
       InputStream is = post(soapMsg);
       return readStream(is);
-    } catch (Exception e) 
+    } catch (Exception e)
     {
       throw new RemoteException("VI SDK invoke exception:" + e);
     }
@@ -145,23 +149,26 @@ final public class WSClient
   public InputStream post(String soapMsg) throws IOException
   {
     HttpURLConnection postCon = (HttpURLConnection) baseUrl.openConnection();
-    
+
     if(connectTimeout > 0)
       postCon.setConnectTimeout(connectTimeout);
     if(readTimeout > 0)
       postCon.setReadTimeout(readTimeout);
-    
+
     try {
         postCon.setRequestMethod("POST");
-    } catch (ProtocolException e) 
+    } catch (ProtocolException e)
     {
         e.printStackTrace();
     }
     postCon.setDoOutput(true);
     postCon.setDoInput(true);
-    postCon.setRequestProperty(SOAP_ACTION_HEADER, soapAction);
+    if (soapAction != null)
+    {
+      postCon.setRequestProperty(SOAP_ACTION_HEADER, soapAction);
+    }
     postCon.setRequestProperty("Content-Type", "text/xml; charset=utf-8");
-    
+
     if(cookie!=null)
     {
       postCon.setRequestProperty("Cookie", cookie);
@@ -174,23 +181,23 @@ final public class WSClient
     out.close();
 
     InputStream is;
-    
+
     try
     {
       is = postCon.getInputStream();
-    } 
+    }
     catch(IOException ioe)
     {
       is = postCon.getErrorStream();
     }
-    
+
     if(cookie==null)
     {
       cookie = postCon.getHeaderField("Set-Cookie");
     }
     return is;
   }
-  
+
   public URL getBaseUrl()
   {
     return this.baseUrl;
@@ -220,7 +227,7 @@ final public class WSClient
   {
     this.vimNameSpace = vimNameSpace;
   }
-  
+
   public void setConnectTimeout(int timeoutMilliSec)
   {
     this.connectTimeout = timeoutMilliSec;
@@ -235,14 +242,14 @@ final public class WSClient
   {
     this.readTimeout = timeoutMilliSec;
   }
-  
+
   public int getReadTimeout()
   {
     return this.readTimeout;
   }
-  
+
 /*===============================================
-   * API versions *   
+   * API versions *
   "2.0.0"    VI 3.0
   "2.5.0"    VI 3.5 (and u1)
   "2.5u2"   VI 3.5u2 (and u3, u4)
@@ -274,49 +281,49 @@ final public class WSClient
       soapAction = SOAP_ACTION_V55;
     }
     else
-    { //always defaults to latest version 
+    { //always defaults to latest version
       soapAction = SOAP_ACTION_V55;
     }
   }
-  
+
   private StringBuffer readStream(InputStream is) throws IOException
   {
     StringBuffer sb = new StringBuffer();
     BufferedReader in = new BufferedReader(new InputStreamReader(is));
     String lineStr;
-    while ((lineStr = in.readLine()) != null) 
+    while ((lineStr = in.readLine()) != null)
     {
       sb.append(lineStr);
     }
     in.close();
     return sb;
   }
-  
-  private static void trustAllHttpsCertificates() 
+
+  private static void trustAllHttpsCertificates()
     throws NoSuchAlgorithmException, KeyManagementException
   {
-    TrustManager[] trustAllCerts = new TrustManager[1]; 
-    trustAllCerts[0] = new TrustAllManager(); 
-    SSLContext sc = SSLContext.getInstance("SSL"); 
-    sc.init(null, trustAllCerts, null); 
+    TrustManager[] trustAllCerts = new TrustManager[1];
+    trustAllCerts[0] = new TrustAllManager();
+    SSLContext sc = SSLContext.getInstance("SSL");
+    sc.init(null, trustAllCerts, null);
     HttpsURLConnection.setDefaultSSLSocketFactory(
         sc.getSocketFactory());
   }
 
-  private static class TrustAllManager implements X509TrustManager 
+  private static class TrustAllManager implements X509TrustManager
   {
-    public X509Certificate[] getAcceptedIssuers() 
+    public X509Certificate[] getAcceptedIssuers()
     {
       return null;
-    } 
-    public void checkServerTrusted(X509Certificate[] certs, 
+    }
+    public void checkServerTrusted(X509Certificate[] certs,
         String authType)
-      throws CertificateException 
+      throws CertificateException
     {
-    } 
-    public void checkClientTrusted(X509Certificate[] certs, 
+    }
+    public void checkClientTrusted(X509Certificate[] certs,
         String authType)
-    throws CertificateException 
+    throws CertificateException
     {
     }
   }
